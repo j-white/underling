fc763880eaeeb2d602c6ff7b945f6f09237be7a0
Jesse White <jesse@opennms.org>
2016-07-26 19:43:40 -0500

    go fmt.

--- underlinglib/detect.go
+++ underlinglib/detect.go
@@ -10,11 +10,11 @@ func SnmpDetect(request DetectorRequestDTO) DetectorResponseDTO {
 
 func Detect(request DetectorRequestDTO) DetectorResponseDTO {
 	switch request.ClassName {
-    case "org.opennms.netmgt.provision.detector.snmp.SnmpDetector":
-        return SnmpDetect(request)
-    case "org.opennms.netmgt.provision.detector.icmp.IcmpDetector":
-        return IcmpDetect(request)
-    default:
-	    return DetectorResponseDTO{Detected: false, FailureMessage: "Unsupported detector class " + request.ClassName}
-    }
+	case "org.opennms.netmgt.provision.detector.snmp.SnmpDetector":
+		return SnmpDetect(request)
+	case "org.opennms.netmgt.provision.detector.icmp.IcmpDetector":
+		return IcmpDetect(request)
+	default:
+		return DetectorResponseDTO{Detected: false, FailureMessage: "Unsupported detector class " + request.ClassName}
+	}
 }
