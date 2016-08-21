cfa3c7abf31ab0c6c5831f011ea6ac645faef1ac
Jesse White <jesse@opennms.org>
2016-08-21 13:27:21 -0400

    Skip the ICMP test on CircleCi.

--- underlinglib/detect_test.go
+++ underlinglib/detect_test.go
@@ -2,12 +2,17 @@ package underlinglib
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestIcmpDetect(t *testing.T) {
-	request := DetectorRequestDTO{Address: "203.0.113.1"}
+	if os.Getenv("CIRCLECI") != "" {
+		t.Skip("skipping test; ICMP not supported in CirclCI.")
+	}
+
+	request := DetectorRequestDTO{Address: "127.0.0.1"}
 	response := IcmpDetect(request)
-	assert.Equal(t, false, response.Detected)
 	assert.Equal(t, "", response.FailureMessage)
+	assert.Equal(t, true, response.Detected)
 }
